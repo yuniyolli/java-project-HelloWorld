9ab01f7ee4c0380497f08ff8c140c5cd927645f2
yuniyolli <yuniyolli@gmail.com>
2023-04-19 12:25:04 +0900

    feat: first commit - java Project 빌드

---
+++ src/main/java/com/example/javaproject2/PrintHello.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2;
+
+public class PrintHello {
+    public void print() {
+        System.out.println("Hello");
+    }
+
+
+
+}
---
+++ src/main/java/com/example/javaproject2/PrintHelloTest.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2;
+
+public class PrintHelloTest {
+    public static void main(String[] args) {
+        PrintHello printHello = new PrintHello(); // 인스턴스화
+        printHello.print();
+
+    }
+}
